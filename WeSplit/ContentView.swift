e658e4efb44f74d18536166ba51f9ca6f1c55b45
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-03-01 15:07:03 +0530

    First Project in Swift

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -7,7 +7,41 @@
 
 import SwiftUI
 
+//struct ContentView: View{
+//    
+//    @State private var checkAmout=0.0
+//    @State private var numberOfPeople=2
+//    @State private var tipAmount=10
+//    
+//    let tipPercentage=[10,15,20,25,0]
+//    
+//    var body: some View{
+//        NavigationStack{
+//            Form{
+//                Section{
+//                    TextField("Enter The Check Amount",value: $checkAmout,format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+//                        .keyboardType(.decimalPad)
+//                }
+//                Section{
+//                    Text(checkAmout,format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+//                }
+//            }
+//            
+//            .navigationTitle("WeSplit")
+//            .navigationBarTitleDisplayMode(.inline)
+//            
+//            
+//        }
+//    }
+//}
+
 struct ContentView: View {
+    @State private var name=""
+    @State private var tapCount=0
+    
+    var arrayOfStudents=["Hitesh","Atul","Honey"]
+    @State private var selectedStudent="Hitesh"
+    
     var body: some View {
 //        VStack {
 //            Image(systemName: "globe")
@@ -17,24 +51,53 @@ struct ContentView: View {
 //        }
 //        .padding()
         
-        Form{
-            Section{
-                Text("Hello World!..")
-            }
-            Text("Hello World!..")
-            Text("Hello World!..")
-            Text("Hello World!..")
-            Text("Hello World!..")
+        NavigationStack{
             
-            Section{
-                Text("Hello World!..")
-                Text("Hello World!..")
-                Text("Hello World!..")
+            Form{
+                Section{
+                    Text("Hello World!..")
+                }
+                
+                Section{
+                    Picker("Select any Student",selection: $selectedStudent){
+                        ForEach(arrayOfStudents,id: \.self){
+                            Text($0)
+                        }
+                    }
+                }
+                
+                
+                TextField("Enter your name",text: $name)
+                Text("This is \(name)")
+                Text("Creating")
+                Text("First SwiftUI")
+                Text("Application")
+                
+                Section{
+                    Text("Form and Sections")
+                    Text("Features of Swift")
+                    Text("🥳🥳🥳🥳🥳")
+                }
+                
+//                ForEach(0..<100){
+//                    Text("No. is \($0)")
+//                }
+                                
             }
-            Text("Hello World!..")
-            Text("Hello World!..")
-            Text("Hello World!..")
+            
+            
+            
+            Button("Tap Count : \(tapCount)"){
+                tapCount += 1
+            }
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
+            
+            
         }
+        
+        
+        
     }
 }
 
