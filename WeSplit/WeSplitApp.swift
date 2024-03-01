748e472b670b4b6e9f1c85858ecacbef1f57ee19
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-03-01 10:08:42 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Hitesh Madaan on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
